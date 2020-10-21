d1994f469e6ccbe64ab701e9a731a84369bf9275
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-10-20 21:02:51 -0500

    update query jobpost scrapp

--- jdrra-back/database/src/controllers/storeFileIntoDB.js
+++ jdrra-back/database/src/controllers/storeFileIntoDB.js
@@ -21,8 +21,8 @@ const storeFileIntoDB = async (res, req) => {
     const jobs = JSON.parse(rawdata);
 
     for (const job of jobs) {
-      const { title, location, company, date, link, senorityLevel, place, jobFunction, employmenttype, industries, description } = job;
-      const response = await pool.query('INSERT INTO jobpost (title, location, company, date, link, senorityLevel, place, jobFunction, employmenttype, industries, description, voteup, votedown) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)', [title, location, company, date, link, senorityLevel, place, jobFunction, employmenttype, industries, description, 0, 0]);
+      const { title, location, company, date, link, senorityLevel, place, jobFunction, employmenttype, industries, description, jobBackTechs, techTypeBack, jobFrontTechs, techTypeFront, jobDevopsTechs, techTypeDevops} = job;
+      const response = await pool.query('INSERT INTO jobpost (title, location, company, date, link, senorityLevel, place, jobFunction, employmenttype, industries, description, jobBackTechs, techTypeBack, jobFrontTechs, techTypeFront, jobDevopsTechs, techTypeDevops, voteup, votedown) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)', [title, location, company, date, link, senorityLevel, place, jobFunction, employmenttype, industries, description, jobBackTechs, techTypeBack, jobFrontTechs, techTypeFront, jobDevopsTechs, techTypeDevops, 0, 0]);
       console.log(response);
     }
   } catch (error) {
