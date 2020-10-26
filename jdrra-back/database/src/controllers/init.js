9748e19c7a5a2666bf6f9537e601b7e1695e5f1f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-10-26 17:33:15 -0500

    update start path text

--- jdrra-back/database/src/controllers/init.js
+++ jdrra-back/database/src/controllers/init.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const getinit = (req, res) => {
-  console.log('Welcome to VagrantDestroyers super secret API\nIf you\'re reading this, probably you are being hacked, have fun XD');
-  res.json('Welcome to VagrantDestroyers super secret API\nIf you\'re reading this, probably you are being hacked, have fun XD')
+  console.log('Welcome to VagrantDestroyers super secret API, If you\'re reading this, probably you are being hacked, have fun :D');
+  res.json('Welcome to VagrantDestroyers super secret API, If you\'re reading this, probably you are being hacked, have fun :D')
 };
 
 module.exports = {
