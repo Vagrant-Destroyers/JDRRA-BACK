78f8728f02574ebf4f21da4519d44af44a757be5
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-10-27 00:14:53 -0500

    fix cors

--- jdrra-back/database/src/index.js
+++ jdrra-back/database/src/index.js
@@ -3,17 +3,20 @@
 
 /* import express */
 const express = require('express');
+var cors = require('cors');
 const port = 9000
 
 /* eject express */
 const app = express();
 
 /* middlewares */
+app.use(cors());
 app.use(express.json());
 
 /* routes */
 app.use(require('../src/routes/index'));
 
 /* listen server on port */
-app.listen(port);
-console.log(`server rum on port: ${port}`)
+app.listen(port, function () {
+    console.log(`CORS-enabled web server listening on port: ${port}`)
+});
